fba47602fa117f013a3270ab5aec1d0ba43b9b3b
dead-weight <57463058+dead-weight@users.noreply.github.com>
2020-09-01 16:57:12 +0300

    v0.3
    Added image support!!

--- src/Components/Placeholder.js
+++ src/Components/Placeholder.js
@@ -4,6 +4,8 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
@@ -14,6 +16,17 @@ import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
 import Button from '@material-ui/core/Button';
 
+import './activeChat.css'
+
+class bruh{
+
+}
+
+class activeChat extends bruh{
+    render() { 
+        return <div className="activeChat"/>
+    }
+}
 
 export const ComponentName = (props) => {
     const {
@@ -22,16 +35,23 @@ export const ComponentName = (props) => {
     
     var temp={
         lol: [
-        {author: 'you',msg:'bruh'},
-        {author: 'not you',msg:'bruh bruh'}
+        {author: 'you',msg:'bruh', img:'', aux:'',vid: ''},
+        {author: 'not you',msg:'bruh bruh', img:'', aux:'',vid: ''},
+        {author: 'not you',msg:'21st century dialogues be like', img:'', aux:'',vid: ''},
+        {author: 'you',msg:'i stapled lobster to a ceiling', img:'', aux:'',vid: ''},
     ],
-    lol2: [],lol3: [],lol4: [],lol5: [],lol6: [],lol7: [],lol8: [],lol9: [],lol10: [],lol11: [],lol12: [],lol13: [],
+    lol2: [
+        {author: 'you', msg:'', img:'https://www.meme-arsenal.com/memes/fd2730642a8e0736a9e0f9ce2d4fed26.jpg', aux:'',vid: ''}
+    ],lol3: [],lol4: [],lol5: [],lol6: [],lol7: [],lol8: [],lol9: [],lol10: [],lol11: [],lol12: [],lol13: [],
     }
     
     const chats = Object.keys(temp)
     const [activeTopic, changeActiveChat] = React.useState(chats[0])
-    
 
+    
+    // const useStyles = makeStyles(theme => ({
+           
+    // }));
     return (
         <div style={{
             backgroundColor: 'grey',
@@ -62,8 +82,8 @@ export const ComponentName = (props) => {
                         <List style={{height: '70vh',overflow: 'auto'}}>
                             {
                                 chats.map(chat => (
-                                    <ListItem onClick={e => changeActiveChat(e.target.innerText)} key={chat} button>
-                                        <ListItemText primary={chat} />
+                                    <ListItem onClick={e => changeActiveChat(e.target.innerText)} key={chat}  button>
+                                        <ListItemText primary={chat}  />
                                     </ListItem>
                                 ))
                             }
@@ -84,16 +104,23 @@ export const ComponentName = (props) => {
                     }}>
                         {
                         // temp.map(x => <div> {JSON.stringify(x)} </div>)
-                        temp[activeTopic].map((msg, i) => (
+                        temp[activeTopic].map((msg, i, j) => (
                             <div key={i}>
                                 {
-                                (msg.author !== 'you')?<Typography variant='body1' style={{textAlign: 'left'}}>{msg.author,msg.msg}</Typography>
+                                (msg.author !== 'you')?<Typography variant='body1' style={{textAlign: 'left', overflow:"hidden"}}>{msg.author,msg.msg}</Typography>
                                 :
-                                <div style={{background:'pink',textAlign: 'right' }}>{msg.author,msg.msg}</div>
+                                <div style={{background:'pink',textAlign: 'right', }}>{msg.author,msg.msg}</div>
                                 }    
-                                
+                            <div key={j} >
+                                {
+                                    (msg.author !== 'you')?<div><img src={msg.img} ></img></div>
+                                    :
+                                    <div><img src={msg.img} alt={msg.alt}></img></div>   
+                                }
+                            </div>    
                                 
                             </div>
+                            
                         ))  
                         }
                            
